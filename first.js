94905c0e0dad585d4f1e00b3993412c0eb29de10
Anubhav Raj <anubhavr219@gmail.com>
2024-12-24 13:22:06 +0530

    removed the years and months only days format

--- first.js
+++ first.js
@@ -6,16 +6,16 @@ function updateCountdown() {
     const timeLeft = olympicDate - now;
 
         
-    const years = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365.25));
-    const months = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30));
-    const days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    // const years = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365.25));
+    // const months = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
         // Update the countdown elements
-        document.querySelector('.years').innerHTML = `<h2>${years} Years</h2>`;
-        document.querySelector('.months').innerHTML = `<h2>${months} Months</h2>`;
+        // document.querySelector('.years').innerHTML = `<h2>${years} Years</h2>`;
+        // document.querySelector('.months').innerHTML = `<h2>${months} Months</h2>`;
         document.querySelector('.days').innerHTML = `<h2>${days} Days</h2>`;
         document.querySelector('.hours').innerHTML = `<h2>${hours} Hours</h2>`;
         document.querySelector('.minutes').innerHTML = `<h2>${minutes} Minutes</h2>`;
